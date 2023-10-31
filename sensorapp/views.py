d69b1ecb85ad7df71a25607708df629637c76358
Euphrosine <euphrosine46@.com>
2023-10-31 07:00:23 -0700

    changes

--- sensorapp/views.py
+++ sensorapp/views.py
@@ -8,14 +8,9 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
 
-
-
-
 def sensor_data_view(request):
-    # Get the value of the 'status' parameter from the URL
     status = request.GET.get('status', None)
 
-    # Initialize param2 (activity) with an empty string
     activity = ""
     sensor = ""
 
@@ -23,18 +18,12 @@ def sensor_data_view(request):
         activity = "Unauthorized person trying to enter the car with fingerprint"
         sensor = "Fingerprint"
     elif status == "2":
-        activity = "Unknown person is coming near your car"
-        sensor = "Aproximity"
-    
-    if status == "3":
         activity = "Unkown person is passing near your car"
         sensor = "Motion"
-
-    elif status == "4":
+    elif status == "3":
         activity = "Unknown person touched your car"
         sensor = "Touch"
-    
-    # Create a dictionary to store the data you want to save
+
     data_to_save = {
         'datetime': timezone.now(),
         'sensor' : sensor,
@@ -42,32 +31,33 @@ def sensor_data_view(request):
         'status': status
     }
 
-    # Create a new entry in the database using the data
     SensorData.objects.create(**data_to_save)
 
-    # Retrieve all entries from the database
     sensor_data = SensorData.objects.all()
 
-    # Convert the data to JSON format
-    response_data = [{'datetime': entry.datetime, 'sensor': entry.sensor,'activity': entry.activity, 'status': entry.status} for entry in sensor_data]
+    # Filter out entries with status "4"
+    response_data = [{'datetime': entry.datetime, 'sensor': entry.sensor,'activity': entry.activity, 'status': entry.status} for entry in sensor_data if entry.status != '4']
 
     return JsonResponse(response_data, safe=False)
+
 
 
 @login_required
 def display_chart_data(request):
     touch_count = SensorData.objects.filter(sensor='Touch').count()
-    aproximity_count = SensorData.objects.filter(sensor='Aproximity').count()
     motion_count = SensorData.objects.filter(sensor='Motion').count()
     fingerprint_count = SensorData.objects.filter(sensor='Fingerprint').count()
-    sensor_data = SensorData.objects.all()
+    sensor_data = SensorData.objects.exclude(sensor='Aproximity')  # Exclude Aproximity
 
-    context = {'sensor_data': sensor_data,
+    context = {
+        'sensor_data': sensor_data,
         'touch_count': touch_count,
-        'aproximity_count': aproximity_count,
         'motion_count': motion_count,
-        'fingerprint_count': fingerprint_count,}
+        'fingerprint_count': fingerprint_count,
+    }
+
     return render(request, 'sensorapp/chart_data_view.html', context)
+
 
 
 from .utils import generate_sensor_data_report
